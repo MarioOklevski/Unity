fd2d1f5b022b22d49adebda5ad38b1c04958cf95
MarkoSimka <63017129+MarkoSimka@users.noreply.github.com>
2020-04-27 21:23:47 +0200

    JAKO

---
+++ First Project/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform Player;
+
+    public GameObject Hero;
+
+    private Rigidbody2D Rb;
+
+    public float Movement_speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = this.GetComponent<Rigidbody2D>();
+        Hero = GameObject.Find("Hero");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Hero.transform.position - transform.position;
+        Debug.Log(direction);
+        Debug.Log(Player.position);
+        Debug.Log(transform.position);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg +90;
+        Rb.rotation = angle;
+    }
+}
