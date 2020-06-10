3099df870642cdab8dec467634db057c690f929a
TrainMKD <62886094+TrainMKD@users.noreply.github.com>
2020-06-10 12:13:15 +0200

    Pause Menu

--- First Project/Assets/Scripts/Menu/MainMenu.cs
+++ First Project/Assets/Scripts/Menu/MainMenu.cs
@@ -6,23 +6,70 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+2);
-    }
-    public void Random(){
-        SceneManager.LoadScene("Begining");
-    }
+    GameObject[] pauseObjects;
 
-    public void SceneToChange(string scene)
-    {
-       SceneManager.LoadScene(scene);
-    }
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 1;
+		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused();
+	}
 
-    public void ExitGame(){
-        Debug.Log("EXIT");
-        Application.Quit();
-    }
+	// Update is called once per frame
+	void Update () {
+
+		//uses the p button to pause and unpause the game
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			if(Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+			} else if (Time.timeScale == 0){
+				Debug.Log ("high");
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
+	}
+
+
+	//Reloads the Level
+	public void Reload(){
+		SceneManager.LoadScene(Application.loadedLevel);
+	}
+
+	//controls the pausing of the scene
+	public void pauseControl(){
+			if(Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+			} else if (Time.timeScale == 0){
+				Time.timeScale = 1;
+				hidePaused();
+			}
+	}
+
+	//shows objects with ShowOnPause tag
+	public void showPaused(){
+		foreach(GameObject g in pauseObjects){
+			g.SetActive(true);
+		}
+	}
+
+	//hides objects with ShowOnPause tag
+	public void hidePaused(){
+		foreach(GameObject g in pauseObjects){
+			g.SetActive(false);
+		}
+	}
+
+	//loads inputted level
+	public void LoadLevel(string level)
+	{
+		SceneManager.LoadScene(level);
+	}
 
 }
  
---
+++ First Project/Assets/Scripts/Pause Menu/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject PauseMenuUI;
+    void Start()
+    {
+        PauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
