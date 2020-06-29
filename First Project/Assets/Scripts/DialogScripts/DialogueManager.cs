4bf51265fe01d3d0c1d732f3748a8ed99c438de3
TrainMKD <62886094+TrainMKD@users.noreply.github.com>
2020-06-29 22:05:11 +0200

    Dialogue And Audio

---
+++ First Project/Assets/Scripts/DialogScripts/DialogueManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.ComponentModel;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI NameText;
+    public TextMeshProUGUI DialogueText;
+
+    public Animator anim;
+
+    public Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue(Dialogue dialogue)
+    {
+        anim.SetBool("IsOpen", true);
+
+        NameText.text = dialogue.Name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        DialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            DialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        anim.SetBool("IsOpen", false);
+    }
+}
---
+++ First Project/Assets/Scripts/Menu/Music.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource TheMusic;
+    void Start()
+    {
+        TheMusic.Play();
+    }
+    public void StopMusic()
+    {
+        TheMusic.Stop();
+    }
+}
